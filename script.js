350fdc39005e056701f1fd2f3c13a65fa3120f86
Souranghu112 <sourangshu098@gmail.com>
2024-05-31 21:49:31 +0530

    Progress of 31 May

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {stickyFunction()};
+
+var header = document.getElementById("image-container");
+var sticky = header.offsetTop;
+
+function stickyFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
